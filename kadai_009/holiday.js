06542012141c8692c37be5ecf8a7dbec86f00f5e
satiko900 <r.ueno.0706@gmail.com>
2024-01-26 22:29:45 +0900

    kadai_009を編集

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,30 +1,22 @@
 // 配列の宣言・値を代入
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
-console.log(holidays);
-
+// while文で記述
+// カウンタ変数の初期値を宣言・代入
 let i = 0;
 
 while (i < holidays.length) {
-  let s = 0;
-  s++;
   console.log(holidays[i]);
-  console.log(holidays[s]);
   i++;
 }
 
-console.log('今のカウント: ' + i);
+// 改行
+console.log('　');
 
-
+// for文で記述
 for (let j =0; j < holidays.length; j++) {
-  console.log(j);
+  console.log(holidays[j]);
 }
 
 
-let age = 0;
-const childLimitAge = 18;
-// 18才まではチケットが買える
-while(age <= 18) {
-  console.log(age + '才なのでチケットが買えます。');
-}
 
