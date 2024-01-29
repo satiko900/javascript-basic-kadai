5e4a07cb44538a8a7682f1f25470d8b2daab7f0f
satiko900 <r.ueno.0706@gmail.com>
2024-01-29 21:23:39 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = () => { 
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  var day = today.getDate();
+  console.log(year + '年' + month + '月' + day + '日');
+};
+
+today();
