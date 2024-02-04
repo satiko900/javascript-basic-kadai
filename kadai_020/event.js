1ec404a9e11ac33ab7a84d3472a639216fdc9cff
satiko900 <r.ueno.0706@gmail.com>
2024-02-04 23:52:44 +0900

    kadai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// textというidを持つHTML要素を取得し、定数に代入
+let Text = document.getElementById('text');
+// btnというidを持つHTML要素を取得し、定数に代入
+const Btn = document.getElementById('btn');
+// NTML要素(btn)がクリックされたときにイベント処理を実行する
+Btn.addEventListener('click', () => {
+  // textの内容を変更
+  Text.textContent = 'ボタンをクリックしました';
+  console.log(Text.textcontent);
+});
